d3d66c6203124f56e0b47d5a72a04c60266f1f8b
bhardwajshraddha <bhardwajshradha800@gmail.com>
2026-09-22 23:50:50 +0530

    Remove "Team Collaboration" from Soft Skills category

--- data/skills.ts
+++ data/skills.ts
@@ -100,7 +100,6 @@ export const skillCategories: SkillCategory[] = [
       "Computer & Internet Troubleshooting",
       "MS Office & Data Handling",
       "Time Management & Multitasking",
-      "Team Collaboration",
     ],
   },
 ];
